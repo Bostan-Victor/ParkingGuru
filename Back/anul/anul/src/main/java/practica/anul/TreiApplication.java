896253793e3776f3bc02fb5695227d8801eaf07d
Bostan Victor <victorbostan01@gmail.com>
2024-09-04 11:34:54 +0300

    init back project

---
+++ Back/anul/anul/src/main/java/practica/anul/TreiApplication.java
@@ -0,0 +1,13 @@
+package practica.anul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TreiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TreiApplication.class, args);
+	}
+
+}
---
+++ Back/anul/anul/src/test/java/practica/anul/TreiApplicationTests.java
@@ -0,0 +1,13 @@
+package practica.anul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TreiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
